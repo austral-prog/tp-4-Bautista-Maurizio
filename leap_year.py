8f0a804070eb262a3c358edabca24e6bf96f5f60
Bautista-Maurizio <83047373+Bautista-Maurizio@users.noreply.github.com>
2024-04-12 11:47:34 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,13 +4,15 @@ def leap_year():
             print(f"El año {year} es bisiesto")
         else:
             print (f"El año {year} no es bisiesto")
-
-    Year = input("Ingrese un año: ")
-    year = int(Year)
-    if (year %100==0):
+    def bisiesto_centenario():
         if (year % 4 == 0) and (year % 400 == 0):
              print(f"El año {year} es bisiesto")
         else:
             print (f"El año {year} no es bisiesto")
+
+    Year = input("Ingrese un año: ")
+    year = int(Year)
+    if (year %100==0):
+        bisiesto_centenario()
     else: bisiesto()
         
